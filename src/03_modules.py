3db5d17a49a4a2d2763128cf99b77d41efd0c8b4
Chris Vasquez <chrvasq@outlook.com>
2020-09-02 20:54:54 -0700

    Complete 00-08

--- src/03_modules.py
+++ src/03_modules.py
@@ -7,25 +7,25 @@ level operating system functionality.
 
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
-# Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
-
-# Print out the OS platform you're using:
-# YOUR CODE HERE
-
-# Print out the version of Python you're using:
-# YOUR CODE HERE
-
-
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
+# Print out the command line arguments in sys.argv, one per line:
+print("Command line arguments passed in:")
+[print(arg) for arg in sys.argv]
+print("...end of list.\n")
+
+# Print out the OS platform you're using:
+print(f"OS platform: {sys.platform}\n")
+
+# Print out the version of Python you're using:
+print(f"Python version: {sys.version}\n")
+
 # Print the current process ID
-# YOUR CODE HERE
+print(f"Current process ID: {os.getpid()}\n")
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(f"Current working directory (cwd): {os.getcwd()}\n")
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(f"Laptop's login name: {os.getlogin()}\n")
--- src/04_printing.py
+++ src/04_printing.py
@@ -1,6 +1,6 @@
 """
 Python provides a number of ways to perform printing. Research
-how to print using the printf operator, the `format` string 
+how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
@@ -8,10 +8,17 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
-# y, and z:
+# Using the printf operator (%), print the following feeding in the values
+# of x, y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+x = 10
+y = 2.25
+z = "I like turtles!"
+
+print("x: %d, y: %.2f, z: %s" % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+print("x: {:d}, y: {:.2f}, z: {!s}".format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print(f"x: {x}, y: {y:.2f}, z: {z}")
